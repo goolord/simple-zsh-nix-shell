e07b96686acbda90eeca2efc94e0f667a64efce5
Zachary Churchill <zacharyachurchill@gmail.com>
2023-02-17 01:15:35 -0500

    init commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Simple ZSH nix shell
+simple wrappers for nix3 and nix2 commands to place you in ZSH when nix would place you in an interactive shell
+
+also adds an icon to $RPS1 when you are in a nix shell
---
+++ simple-nix-shell.zsh
@@ -0,0 +1,52 @@
+function in_nix_shell() {
+  if [ ! -z ${IN_NIX_SHELL+x} ];
+    then echo " nix ";
+  fi
+}
+
+RPS1="%F{yellow}%b$(in_nix_shell)%f$RPS1"
+
+check_arg() {
+  arg="$1"
+  shift
+  for i in "$@" ; do
+    if [[ $i == $arg ]] ; then
+      echo "1"
+      return 1
+    fi
+  done
+}
+
+nix() {
+  subcommand="$1"
+  shift
+  case $subcommand in
+    develop|shell|run)
+      has_arg=$(check_arg "--command" "$@")
+      if [ has_arg != "1" ];
+      then
+        command nix "$subcommand" "$@" --command zsh
+      else
+        command nix "$subcommand" "$@"
+      fi
+      ;;
+    *)
+      command nix "$@";;
+  esac
+}
+
+nix-shell() {
+  case $@ in
+    "")
+      has_arg=$(check_arg "--run" "$@")
+      if [ has_arg != "1" ];
+      then
+        command nix-shell --run zsh
+      else
+        command nix-shell "$@"
+      fi
+      ;;
+    *)
+      command nix-shell "$@";;
+  esac
+}
