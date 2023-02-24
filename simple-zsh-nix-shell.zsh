56756304973ddcb17896337d0bacaa9f65423699
Zachary Churchill <zacharyachurchill@gmail.com>
2023-02-24 14:12:44 -0500

    simplify

--- simple-zsh-nix-shell.zsh
+++ simple-zsh-nix-shell.zsh
@@ -6,15 +6,4 @@ function in_nix_shell() {
 
 RPS1="%F{yellow}%b$(in_nix_shell)%f$RPS1"
 
-check_arg() {
-  arg="$1"
-  shift
-  for i in "$@" ; do
-    if [[ $i == $arg ]] ; then
-      echo "1"
-      return 1
-    fi
-  done
-}
-
 export SIMPLE_ZSH_NIX_SHELL_BASH=${0:A:h}/bash.sh
